d594fea662fc275ca70f83cc92e13e174d839cd2
TwaroRPI <mattwar1@github.com>
2021-12-19 21:18:45 +0100

    MID_DEV_v1

--- MID_DEV/mid_dev_v1.py
+++ MID_DEV/mid_dev_v1.py
@@ -11,8 +11,11 @@
 # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
+#!/usr/bin/env python
 from digi.xbee.devices import XBeeDevice
+import http.client
+import json
+from os import stat
 
 # TODO: Replace with the serial port where your local module is connected to.
 PORT = "/dev/ttyUSB0"
@@ -28,15 +31,27 @@ def main():
     
     device = XBeeDevice(PORT, BAUD_RATE)
 
+     
+    headers = {"Content-type": "application/json"}
+    conn = http.client.HTTPConnection("192.168.1.107:5000")
     try:
         device.open()
-
         def data_receive_callback(xbee_message):
             print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(),
                                      xbee_message.data.decode()))
-
+            MAC = xbee_message.remote_device.get_64bit_addr() 
+            stat = xbee_message.data.decode()
+            conn.request("GET", "/v1/parking/0")
+            resp = conn.getresponse()
+            data = json.loads(resp.read())
+            floor = data["floor"]
+            params = json.dumps({"place":str(MAC),"status":int(stat)})
+            if MAC in floor.keys():
+                conn.request("POST","/v1/parking/0", params, headers)
+            else:
+                conn.request("POST","/v1/parking/0/"+str(MAC), params, headers)
+            response = conn.getresponse()
         device.add_data_received_callback(data_receive_callback)
-
         print("Waiting for data...\n")
         input()
 
